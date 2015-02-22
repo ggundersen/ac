3cdef84f9a0e0d5c9119adbd8f200c28304a84c7
gwg <ggundersen@gmail.com>
2015-02-22 16:41:16 -0500

    Cleanup

--- src/Song.java
+++ src/Song.java
@@ -8,26 +8,25 @@ public class Song {
     private static final String[] day2ordinal;
     static {
         day2gift = new String[] {
-            "a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
-            "calling Birds", "gold rings", "geese a-laying",
-            "swans a-swimming", "maids a-milking", "ladies dancing",
-            "lords a-leaping", "pipers piping", "drummers drumming"
+            "a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "calling Birds",
+            "gold rings", "geese a-laying", "swans a-swimming", "maids a-milking",
+            "ladies dancing", "lords a-leaping", "pipers piping", "drummers drumming"
         };
         day2ordinal = new String[] {
-            "first", "second", "third",
-            "fourth", "fifth", "sixth",
-            "seventh", "eighth", "ninth",
-            "tenth", "eleventh", "twelfth"
+            "first", "second", "third", "fourth",
+            "fifth", "sixth", "seventh", "eighth",
+            "ninth", "tenth", "eleventh", "twelfth"
         };
     }
 
     private static void stanza(int day) {
+        String indent = "  ";
         System.out.println("On the " + day2ordinal[day] + " day of Christmas my true love gave to me");
         for (int i = day; i >= 0; i--) {
             if (i == 0 && day != 0)
-                System.out.println("  and " + day2gift[i]);
+                System.out.println(indent + "and " + day2gift[i]);
             else
-                System.out.println("  " + day2gift[i]);
+                System.out.println(indent + day2gift[i]);
         }
     }
 
